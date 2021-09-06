4f9ca49a58122fd9a133b070bc171c3383fa8d6d
eclipseeer <eclipseeer@gmail.com>
2021-09-06 12:33:51 +0300

    Fix add_keys

--- near-campaign/src/add_keys.rs
+++ near-campaign/src/add_keys.rs
@@ -5,12 +5,11 @@ impl Campaign {
   #[private]
   pub fn add_keys(&mut self, keys: Vec<PublicKey>) {
     // TODO this doesn't handle overlap of keys that already exist in the map?
-    self.keys_stats.total += keys.len() as u64;
-
     keys.into_iter().for_each(|key| {
       // TODO do we need to check if key is already added to the state?
       // TODO: use callback for this?
       self.keys.insert(&key, &KeyStatus::Active);
+      self.keys_stats.total += 1;
       self.keys_stats.active += 1;
 
       Promise::new(env::current_account_id()).add_access_key(
