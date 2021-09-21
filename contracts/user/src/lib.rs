91588af37703cc72d3bface2d9bfd049c85c61c6
eclipseeer <eclipseeer@gmail.com>
2021-09-21 10:15:00 +0300

    Add user version

--- contracts/user/src/lib.rs
+++ contracts/user/src/lib.rs
@@ -6,6 +6,7 @@ use near_sdk::{env, ext_contract, near_bindgen, AccountId, PanicOnDefault, Promi
 
 mod create_near_campaign;
 mod get_campaigns;
+mod get_user_metadata;
 mod new;
 mod on_near_campaign_created;
 mod on_near_campaign_deleted;
@@ -22,4 +23,5 @@ pub trait ExtSelfUser {
 pub struct User {
   future_campaign_id: u64,
   campaigns: UnorderedSet<AccountId>,
+  version: String,
 }
--- tests/sim/dev_test.rs
+++ tests/sim/dev_test.rs
@@ -55,48 +55,51 @@ fn dev_test() {
   let (root, runtime) = init_simulation();
   let user = deploy_user_contract(&root, "bob");
 
+  let view1 = view!(user.get_user_metadata());
+  dbg!(view1.unwrap_json_value());
+
   // Create a new campaign
-  let call1 = call!(
-    user.user_account,
-    user.create_near_campaign(
-      "my_campaign".to_string(),
-      PublicKey::from_str(PK).unwrap(),
-      5,
-      U128::from(to_yocto("1")),
-      "testnet-1".parse().unwrap()
-    ),
-    deposit = to_yocto("50")
-  );
+  // let call1 = call!(
+  //   user.user_account,
+  //   user.create_near_campaign(
+  //     "my_campaign".to_string(),
+  //     PublicKey::from_str(PK).unwrap(),
+  //     5,
+  //     U128::from(to_yocto("1")),
+  //     "testnet-1".parse().unwrap()
+  //   ),
+  //   deposit = to_yocto("50")
+  // );
   // Check user campaigns
   // let view1 = view!(user.get_campaigns());
   // dbg!(view1.unwrap_json_value());
 
 
-  let my_campaign = get_contract_account(
-    "my_campaign.bob",
-    SK,
-    runtime.clone(),
-    CampaignContract {
-      account_id: "my_campaign.bob".parse().unwrap(),
-    },
-  );
+  // let my_campaign = get_contract_account(
+  //   "my_campaign.bob",
+  //   SK,
+  //   runtime.clone(),
+  //   CampaignContract {
+  //     account_id: "my_campaign.bob".parse().unwrap(),
+  //   },
+  // );
   // let view1 = view!(my_campaign.get_campaign_metadata());
   // dbg!(view1.unwrap_json_value());
 
   // Add keys
-  let key_set = KeySet::create(0, 4);
-
-  let add_keys_result = call!(
-    my_campaign.user_account,
-    my_campaign.add_keys(key_set.public_keys())
-  );
+  // let key_set = KeySet::create(0, 4);
+  //
+  // let add_keys_result = call!(
+  //   my_campaign.user_account,
+  //   my_campaign.add_keys(key_set.public_keys())
+  // );
 
 
   // Refund keys
-  let refund_keys_result = call!(
-    my_campaign.user_account,
-    my_campaign.refund_keys(key_set.public_keys(), user.account_id())
-  );
+  // let refund_keys_result = call!(
+  //   my_campaign.user_account,
+  //   my_campaign.refund_keys(key_set.public_keys(), user.account_id())
+  // );
   // dbg!(refund_keys_result);
   // let view2 = view!(my_campaign.get_campaign_metadata());
   // dbg!(view2.unwrap_json_value());
